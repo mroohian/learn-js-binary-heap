ffe00c181e37c1711cdd66298903a0b3a808e501
mroohian <m.roohian87@gmail.com>
2021-04-19 18:04:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-binary-heap
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-binary-heap)
---
+++ script.js
@@ -0,0 +1,14 @@
+import { BinaryHeap } from "./BinaryHeap";
+
+console.clear();
+
+const heap = new BinaryHeap();
+[10, 3, 4, 8, 2, 9, 7, 1, 6, 5].forEach(x => heap.push(x));
+
+heap.remove(2);
+
+heap.push(5.5);
+
+while (heap.size() > 0) {
+  console.log(heap.pop());
+}
